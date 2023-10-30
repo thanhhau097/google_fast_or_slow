bd39c6eda0ecae154e109749cf8454d03b8f1840
Thanh Hau Nguyen <thanhhau097@gmail.com>
2023-10-30 14:16:47 +0000

    add losses

--- engine.py
+++ engine.py
@@ -4,7 +4,7 @@ from typing import Dict
 import numpy as np
 import torch
 import torch.nn.functional as F
-from pytorchltr.loss import PairwiseHingeLoss
+from loss import PairwiseHingeLoss
 from scipy.stats import kendalltau
 from torch import nn
 from transformers import Trainer
@@ -20,51 +20,6 @@ def pairwise_hinge_loss(y_pred, y_true):
     return loss_fn(
         y_pred, y_true, n=torch.tensor([y_pred.shape[1]], device=y_pred.device)
     ).mean()
-
-
-# https://github.dev/allegro/allRank/blob/master/allrank/models/losses/listMLE.py
-# TODO: not converged yet
-def listMLE(y_pred, y_true, eps=1e-10, padded_value_indicator=-float("inf")):
-    """
-    ListMLE loss introduced in "Listwise Approach to Learning to Rank - Theory and Algorithm".
-    :param y_pred: predictions from the model, shape [batch_size, slate_length]
-    :param y_true: ground truth labels, shape [batch_size, slate_length]
-    :param eps: epsilon value, used for numerical stability
-    :param padded_value_indicator: an indicator of the y_true index containing a padded item, e.g. -1
-    :return: loss value, a torch.Tensor
-    """
-    # shuffle for randomised tie resolution
-    if len(y_pred.shape) == 1:
-        y_pred = y_pred.unsqueeze(0)
-
-    if len(y_true.shape) == 1:
-        y_true = y_true.unsqueeze(0)
-
-    random_indices = torch.randperm(y_pred.shape[-1])
-
-    y_pred_shuffled = y_pred[:, random_indices]
-    y_true_shuffled = y_true[:, random_indices]
-
-    y_true_sorted, indices = y_true_shuffled.sort(descending=True, dim=-1)
-
-    mask = y_true_sorted == padded_value_indicator
-
-    preds_sorted_by_true = torch.gather(y_pred_shuffled, dim=1, index=indices)
-    preds_sorted_by_true[mask] = float("-inf")
-
-    max_pred_values, _ = preds_sorted_by_true.max(dim=1, keepdim=True)
-
-    preds_sorted_by_true_minus_max = preds_sorted_by_true - max_pred_values
-
-    cumsums = torch.cumsum(
-        preds_sorted_by_true_minus_max.exp().flip(dims=[1]), dim=1
-    ).flip(dims=[1])
-
-    observation_loss = torch.log(cumsums + eps) - preds_sorted_by_true_minus_max
-
-    observation_loss[mask] = 0.0
-
-    return torch.mean(torch.sum(observation_loss, dim=1))
 
 
 class CustomTrainer(Trainer):
