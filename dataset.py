9f4556421595b42df0ecca16c20eec5ef252fa12
Eduardo Rocha de Andrade <du-ra92@hotmail.com>
2023-11-09 13:58:42 -0300

    scaler

--- dataset.py
+++ dataset.py
@@ -331,18 +331,23 @@ class DatasetFactory:
         )
 
         self.dataset_cls = LayoutDataset if data_type == "layout" else TileDataset
-        all_data = pd.concat([self.train_df, self.valid_df, self.test_df])
+
         # Create dummy dataset just to get the scaler in a deterministic way
+        all_data = pd.concat([self.train_df, self.valid_df, self.test_df])
         dummy_dataset = self.dataset_cls(
             all_data,
             split="train",
             max_configs=self.max_configs,
             scaler=self.scaler,
-            tgt_scaler=self.tgt_scaler,
-            select_close_runtimes=self.select_close_runtimes,
-            select_close_runtimes_prob=self.select_close_runtimes_prob,
         )
         self._scaler_obj = dummy_dataset.scaler
+        all_data = pd.concat([self.train_df, self.valid_df])
+        dummy_dataset = self.dataset_cls(
+            all_data,
+            split="train",
+            max_configs=self.max_configs,
+            tgt_scaler=self.tgt_scaler,
+        )
         self._tgt_scaler_obj = dummy_dataset.tgt_scaler
         if kfold:
             # Create kfold splits
@@ -369,20 +374,28 @@ class DatasetFactory:
                 select_close_runtimes=self.select_close_runtimes,
                 select_close_runtimes_prob=self.select_close_runtimes_prob,
             )
+            train_dataset.scaler = self._scaler_obj
+            train_dataset.tgt_scaler = self._tgt_scaler_obj
+
             valid_dataset = self.dataset_cls(
                 self.valid_df,
                 split="valid",
                 max_configs=self.max_configs_eval,
             )
-            valid_dataset.scaler = self._scaler_obj  # train_dataset.scaler
-            valid_dataset.tgt_scaler = self._tgt_scaler_obj  # train_dataset.tgt_scaler
+            valid_dataset.scaler = self._scaler_obj
+            valid_dataset.tgt_scaler = self._tgt_scaler_obj
+            # valid_dataset.scaler = train_dataset.scaler
+            # valid_dataset.tgt_scaler = train_dataset.tgt_scaler
+
             test_dataset = self.dataset_cls(
                 self.test_df,
                 split="test",
                 max_configs=self.max_configs_eval,
             )
-            test_dataset.scaler = self._scaler_obj  # train_dataset.scaler
-            test_dataset.tgt_scaler = self._tgt_scaler_obj  # train_dataset.tgt_scaler
+            test_dataset.scaler = self._scaler_obj
+            test_dataset.tgt_scaler = self._tgt_scaler_obj
+            # test_dataset.scaler = train_dataset.scaler
+            # test_dataset.tgt_scaler = train_dataset.tgt_scaler
         else:
             train_dataset = self.dataset_cls(
                 self.all_data[self.all_data["fold"] != fold],
@@ -395,6 +408,7 @@ class DatasetFactory:
             )
             train_dataset.scaler = self._scaler_obj
             train_dataset.tgt_scaler = self._tgt_scaler_obj
+
             valid_dataset = self.dataset_cls(
                 self.all_data[self.all_data["fold"] == fold],
                 split="valid",
@@ -404,6 +418,7 @@ class DatasetFactory:
             valid_dataset.tgt_scaler = self._tgt_scaler_obj
             # valid_dataset.scaler = train_dataset.scaler
             # valid_dataset.tgt_scaler = train_dataset.tgt_scaler
+
             test_dataset = self.dataset_cls(
                 self.test_df,
                 split="test",
@@ -413,6 +428,7 @@ class DatasetFactory:
             test_dataset.tgt_scaler = self._tgt_scaler_obj
             # test_dataset.scaler = train_dataset.scaler
             # test_dataset.tgt_scaler = train_dataset.tgt_scaler
+
             # Dump scalers to disk
             if output_dir is not None:
                 save_scaler(train_dataset.scaler, str(Path(output_dir) / "scaler.pkl"))
